3fed54c1f7f24c4c0bbe1add30543982c43cf261
schou <pschou@users.noreply.github.com>
2022-05-26 22:49:32 -0400

    add flags and readme blurb

--- README.md
+++ README.md
@@ -1,11 +1,74 @@
 # RPM Add SHA256
 
-A very basic tool to compute SHA256 hashes for rpm files to enable FIPS compatibility.
+A very basic tool to compute and surgically add SHA256 hashes to the header of
+an rpm files to enable FIPS compatibility.  No other bytes in the rpm file are
+changed so the updated and original RPM function exactly the same.  While this
+tool enhances the digest / checksums inside an existing RPM file, it should
+only be used when a chain of custody can be verified out-of-band as the intent
+of enabling FIPS on a system is to ensure that a higher level of digest checks
+are done and thus a higher level of confidence that the files have not been
+tampered with.  Adding a checksum with this tool adds this confidence-- act
+wisely as with great power comes great responsibility.
 
 ```bash
 $ ./rpm-add-sha256 -h
-rpm-add-sha256,  Version: 0.1.20220523.1524 (https://github.com/pschou/rpm-add-sha256)
+rpm-add-sha256,  Version: 0.1.20220526.2240 (https://github.com/pschou/rpm-add-sha256)
 
-Usage: ./rpm-add-sha256 input.rpm output.rpm
+Usage: rpm-add-sha256 [options] input.rpm output.rpm
 
+  -f    Force new file to be written, even if checksum fails
+  -i    Do upgrade of file hashes in place
+  -t    Test if SHA256 is present in input file
+  -v    Turn on verbose
+```
+
+## Examples
+
+Adding a sha256 header to a file:
+```bash
+$ ./rpm-add-sha256 -v -i hello.rpm
+reading package to hash
+Finished hashing package:
+  f5a36aea3bfdf2206eb8babfc42b1f42606db7c9
+  6a3a905fab31fe49105203044b55124ef870489aef7028a6abbb3e76338ee55c
+Payload header:
+  adding 6a3a905fab31fe49105203044b55124ef870489aef7028a6abbb3e76338ee55c
+Computing new header hash:
+  sha1: 4a8dc6e3015afaade19cca47b61caf1a5b4ffbbe
+  sha256: cf9ecbf0c0389948a463811b9ef3ebdf1a64314c03c1277b242e0d729816d652
+  md5: _XiGKKOepeIaKm5GNHjUrQ==
+  original md5: vA7he-bCNai-4zosehFhQg==
+Signature headers:
+  updated sha1, prev: e90be5a077c965790392fb78364f2354c9bf9884
+  added sha256
+  Updated size, prev: [4130] now 4234 = 260 2640 1594
+  Updated md5, prev: vA7he-bCNai-4zosehFhQg== now _XiGKKOepeIaKm5GNHjUrQ==
+```
+
+File already had sha256 header:
+```bash
+$ ./rpm-add-sha256 -v -i hello.rpm
+Found Digest SHA256
+```
+
+When the internal checksum fails, the sha256 upgrade is not added:
+```bash
+$ ./rpm-add-sha256 -v -i  hello.rpm
+reading package to hash
+Finished hashing package:
+  c19346950f20d5051373982115daf833aa2a993c
+  b8fa954a8a61796b288d54a4e154ffb68688f7abfb1d3eb4eb9f9647e4ccacde
+Payload header:
+  adding b8fa954a8a61796b288d54a4e154ffb68688f7abfb1d3eb4eb9f9647e4ccacde
+Computing new header hash:
+  sha1: 0efbf5fc728638ef90d675564406d9286fb5db4e
+  sha256: dcb2e8a128e192d23ac7bfc1401012ca48d39a07dd7f386041e59a3294e471c5
+  md5: hDTjiJ8PQ8JmCmMv_X3BtQ==
+  original md5: 9jkEXouLBiMWxDunhFvgvg==
+Signature headers:
+  updated sha1, prev: e90be5a077c965790392fb78364f2354c9bf9884
+  added sha256
+  Updated size, prev: [4130] now 4234 = 260 2640 1594
+  Updated md5, prev: vA7he-bCNai-4zosehFhQg== now hDTjiJ8PQ8JmCmMv_X3BtQ==
+Error verifying internal MD5 checksum--
 ```
--- main.go
+++ main.go
@@ -28,6 +28,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 
 	rpm "github.com/pschou/go-rpm"
 )
@@ -36,20 +37,40 @@ var version string
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "rpm-add-sha256,  Version: %s (https://github.com/pschou/rpm-add-sha256)\n\nUsage: %s input.rpm output.rpm\n\n", version, os.Args[0])
+		_, f := path.Split(os.Args[0])
+		fmt.Fprintf(os.Stderr, "rpm-add-sha256,  Version: %s (https://github.com/pschou/rpm-add-sha256)\n\n"+
+			"Usage: %s [options] input.rpm output.rpm\n\n", version, f)
 		flag.PrintDefaults()
 	}
 
 	log.SetFlags(0)
 	log.SetPrefix("rpm-add-sha256: ")
-	test := flag.Bool("test", false, "Test if SHA256 is present in input file")
+	test := flag.Bool("t", false, "Test if SHA256 is present in input file")
+	force := flag.Bool("f", false, "Force new file to be written, even if checksum fails")
+	inplace := flag.Bool("i", false, "Do upgrade of file hashes in place")
+	verbose := flag.Bool("v", false, "Turn on verbose")
+	//padding := flag.Bool("a", false, "Add reserve space for signing")
 
 	flag.Parse()
 
-	if *test && flag.NArg() >= 1 {
+	var outFile string
+	if *test {
+		// Test only
+		if flag.NArg() != 1 {
+			fmt.Println("Input file to test needed")
+			os.Exit(1)
+		}
+	} else if *inplace {
+		if flag.NArg() != 1 {
+			fmt.Println("Input file to upgrade needed")
+			os.Exit(1)
+		}
+		outFile = flag.Arg(0) + ".inplace"
 	} else if flag.NArg() != 2 {
 		fmt.Println("Input and Output file needed")
 		os.Exit(1)
+	} else {
+		outFile = flag.Arg(1)
 	}
 
 	fi, err := os.Open(flag.Arg(0))
@@ -70,10 +91,10 @@ func main() {
 	//fmt.Println("Found", string(ln))
 
 	var (
-		hdr             *rpm.Header
-		hdrs            []*rpm.Header
-		found_file_dgst bool
-		file_dgst_algo  []uint32
+		hdr                *rpm.Header
+		hdrs               []*rpm.Header
+		found_payload_dgst bool
+		payload_dgst_algo  []uint32
 	)
 
 	for i := 0; i < 2; i++ {
@@ -85,20 +106,20 @@ func main() {
 		//fmt.Println("HDR Found", string(ln))
 		//fmt.Println("")
 		for _, t := range hdr.Tags {
-			if t.Tag == rpm.RPMTAG_FILEDIGESTALGO {
-				found_file_dgst = true
-				file_dgst_algo, _ = t.Int32()
-				fmt.Printf("tag: %#v\n", t)
+			if t.Tag == rpm.RPMTAG_PAYLOADDIGESTALGO {
+				found_payload_dgst = true
+				payload_dgst_algo, _ = t.Int32()
+				//fmt.Printf("tag: %#v\n", t)
 			}
 		}
 		//fmt.Printf("Writing header: %#v\n", hdr)
 		hdrs = append(hdrs, hdr)
 	}
 
-	if *test {
-		if found_file_dgst {
+	if *test || (*inplace && found_payload_dgst && !*force) {
+		if found_payload_dgst {
 			algo := ""
-			switch file_dgst_algo[0] {
+			switch payload_dgst_algo[0] {
 			case rpm.PGPHASHALGO_MD5:
 				algo = "MD5"
 			case rpm.PGPHASHALGO_SHA1:
@@ -117,7 +138,9 @@ func main() {
 		log.Fatal("unable to determine current offset")
 	}
 
-	fmt.Println("reading package to hash")
+	if *verbose {
+		fmt.Println("reading package to hash")
+	}
 	h_sha1 := sha1.New()
 	h_sha256 := sha256.New()
 	hashes := io.MultiWriter(h_sha1, h_sha256)
@@ -128,26 +151,34 @@ func main() {
 		fmt.Println("err to hash")
 		log.Fatal(err)
 	}
-	fmt.Println("Finished hashing package:")
-	fmt.Printf("  %0x\n", h_sha1.Sum(nil))
-	fmt.Printf("  %0x\n", h_sha256.Sum(nil))
+	if *verbose {
+		fmt.Println("Finished hashing package:")
+		fmt.Printf("  %0x\n", h_sha1.Sum(nil))
+		fmt.Printf("  %0x\n", h_sha256.Sum(nil))
+	}
 
-	//if !found_payload_dgst && found_file_dgst {
+	//if !found_payload_dgst && found_payload_dgst {
 	//}
-	fmt.Println("Payload header:")
+	if *verbose {
+		fmt.Println("Payload header:")
+	}
 	payloadHdr := rpm.NewPayloadHeader()
 	for _, tag := range hdrs[1].Tags {
 		switch tag.Tag {
 		case rpm.RPMTAG_PAYLOADDIGESTALGO:
 		case rpm.RPMTAG_PAYLOADDIGEST:
 			d, _ := tag.StringData()
-			fmt.Println("  Removing", d)
+			if *verbose {
+				fmt.Println("  Removing", d)
+			}
 		default:
 			payloadHdr.Add(tag)
 		}
 	}
 	payloadHdr.AddStringArray(rpm.RPMTAG_PAYLOADDIGEST, hex.EncodeToString(h_sha256.Sum(nil)))
-	fmt.Println("  adding", hex.EncodeToString(h_sha256.Sum(nil)))
+	if *verbose {
+		fmt.Println("  adding", hex.EncodeToString(h_sha256.Sum(nil)))
+	}
 	payloadHdr.AddInt32(rpm.RPMTAG_PAYLOADDIGESTALGO, rpm.PGPHASHALGO_SHA256)
 	//fmt.Println("adding payload digest to header:", hex.EncodeToString(h_sha256.Sum(nil)))
 	//ln, _ := hdr.MarshalJSON()
@@ -158,7 +189,9 @@ func main() {
 	//}
 
 	// Create headers buffer
-	fmt.Println("Computing new header hash:")
+	if *verbose {
+		fmt.Println("Computing new header hash:")
+	}
 	headers := bytes.NewBufferString("")
 	header_sha1 := sha1.New()
 	header_sha256 := sha256.New()
@@ -169,20 +202,33 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Compare with current infile header
+	orig_header_md5 := md5.New()
+	rpm.WriteHeaders(orig_header_md5, hdrs[1])
+
 	fi.Seek(offset, io.SeekStart)
-	io.Copy(header_md5, fi)
+	payload_hasher := io.MultiWriter(orig_header_md5, header_md5)
+	io.Copy(payload_hasher, fi)
 
-	fmt.Println("  sha1:", hex.EncodeToString(header_sha1.Sum(nil)))
-	fmt.Println("  sha256:", hex.EncodeToString(header_sha256.Sum(nil)))
-	fmt.Println("  md5:", b64.URLEncoding.EncodeToString(header_md5.Sum(nil)))
+	if *verbose {
+		fmt.Println("  sha1:", hex.EncodeToString(header_sha1.Sum(nil)))
+		fmt.Println("  sha256:", hex.EncodeToString(header_sha256.Sum(nil)))
+		fmt.Println("  md5:", b64.URLEncoding.EncodeToString(header_md5.Sum(nil)))
+		fmt.Println("  original md5:", b64.URLEncoding.EncodeToString(orig_header_md5.Sum(nil)))
+	}
+	orig_md5_compare := b64.URLEncoding.EncodeToString(orig_header_md5.Sum(nil))
 
-	fmt.Println("Signature headers:")
+	if *verbose {
+		fmt.Println("Signature headers:")
+	}
 	// Look to see is SHA256 is already declared
 	Tags := hdrs[0].Tags
 	var has_header_sha256 bool
 	for _, tag := range Tags {
 		if tag.Tag == rpm.RPMTAG_SHA256HEADER {
-			fmt.Println("  found sha256 in header")
+			if *verbose {
+				fmt.Println("  found sha256 in header")
+			}
 			has_header_sha256 = true
 		}
 	}
@@ -190,18 +236,27 @@ func main() {
 	// Go ahead and declare it
 	newHdr := rpm.NewSignatureHeader()
 	for _, tag := range Tags {
+		//if *padding && tag.Tag == rpm.RPMSIGTAG_RESERVEDSPACE {
+		//	continue
+		//}
 		switch tag.Tag {
 		case rpm.RPMTAG_SHA1HEADER:
 			d, _ := tag.StringData()
-			fmt.Println("  updated sha1, prev:", string(d))
+			if *verbose {
+				fmt.Println("  updated sha1, prev:", string(d))
+			}
 			newHdr.AddString(rpm.RPMTAG_SHA1HEADER, hex.EncodeToString(header_sha1.Sum(nil)))
 			if !has_header_sha256 {
-				fmt.Println("  added sha256")
+				if *verbose {
+					fmt.Println("  added sha256")
+				}
 				newHdr.AddString(rpm.RPMTAG_SHA256HEADER, hex.EncodeToString(header_sha256.Sum(nil)))
 			}
 		case rpm.RPMTAG_SHA256HEADER:
 			d, _ := tag.StringData()
-			fmt.Println("  updated sha256, prev:", string(d))
+			if *verbose {
+				fmt.Println("  updated sha256, prev:", string(d))
+			}
 			newHdr.AddString(rpm.RPMTAG_SHA256HEADER, hex.EncodeToString(header_sha256.Sum(nil)))
 		default:
 			newHdr.Add(tag)
@@ -212,27 +267,47 @@ func main() {
 	var signatureHdrSize int64
 	signatureHdrSize, _ = rpm.WriteHeaders(ioutil.Discard, newHdr)
 
+	var upgrade_check bool
+
 	Tags = newHdr.Tags
 	// Write out the new headers with size
 	newHdr = rpm.NewSignatureHeader()
 	for _, tag := range Tags {
 		switch tag.Tag {
 		case rpm.RPMSIGTAG_PGP, rpm.RPMTAG_RSAHEADER:
-			fmt.Println("  removing:", tag)
+			if *verbose {
+				fmt.Println("  removing:", tag)
+			}
 		case rpm.RPMSIGTAG_SIZE:
 			d, _ := tag.Int32()
-			fmt.Println("  Updated size, prev:", d, "now", uint32(payloadHdrSize+pkgSize), "=", signatureHdrSize, payloadHdrSize, pkgSize)
+			if *verbose {
+				fmt.Println("  Updated size, prev:", d, "now", uint32(payloadHdrSize+pkgSize), "=", signatureHdrSize, payloadHdrSize, pkgSize)
+			}
 			newHdr.AddInt32(rpm.RPMSIGTAG_SIZE, uint32(payloadHdrSize+pkgSize))
 		case rpm.RPMSIGTAG_MD5:
 			d, _ := tag.Bytes()
-			fmt.Println("  Updated md5, prev:", b64.URLEncoding.EncodeToString(d), "now", b64.URLEncoding.EncodeToString(header_md5.Sum(nil)))
+			if *verbose {
+				fmt.Println("  Updated md5, prev:", b64.URLEncoding.EncodeToString(d), "now", b64.URLEncoding.EncodeToString(header_md5.Sum(nil)))
+			}
 			newHdr.AddBin(rpm.RPMSIGTAG_MD5, header_md5.Sum(nil))
+			if b64.URLEncoding.EncodeToString(d) == orig_md5_compare {
+				upgrade_check = true
+			}
 		default:
 			newHdr.Add(tag)
 		}
 	}
 
-	fo, err := os.Create(flag.Arg(1))
+	if !upgrade_check {
+		if !*force {
+			fmt.Println("Error verifying internal MD5 checksum--")
+			os.Exit(1)
+		} else {
+			fmt.Println("Warning, file creation forced with checksum mismatch!")
+		}
+	}
+
+	fo, err := os.Create(outFile)
 
 	if _, err = lead.WriteTo(fo); err != nil {
 		log.Fatal(err)
@@ -270,5 +345,8 @@ func main() {
 
 	if err != nil && !errors.Is(err, io.EOF) {
 		log.Fatalf("error: %v", err)
+	}
+	if *inplace {
+		os.Rename(outFile, flag.Arg(0))
 	}
 }
